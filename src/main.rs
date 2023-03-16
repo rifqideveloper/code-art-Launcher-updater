ce18beec105da7d81072ddbdaa36f60f6e13a12b
rifqideveloper <zombititi@gmail.com>
2023-03-16 15:49:50 +0700

    mulai ulang

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,23 @@
 
 use std::process::Command;
 fn main() {
-    if cfg!(target_os = "windows") {
-        std::fs::rename("./codeartstudio.exe.update", "./codeartstudio.exe").ok();
-        std::fs::rename("./codeartstudio.pck.update", "./codeartstudio.pck").ok();
-        Command::new("codeartstudio").output().expect("failed to execute process");
-    } else {
+    loop {
+        if cfg!(target_os = "windows") {
+            std::fs::rename("./codeartstudio.exe.update", "./codeartstudio.exe").ok();
+            std::fs::rename("./codeartstudio.pck.update", "./codeartstudio.pck").ok();
+            
+            if let Ok(c) = Command::new("codeartstudio").status()  {
+				if let Some(r) = c.code() {
+					if r == 12 {
+						continue
+					}
+					
+				}
+            }
+        
+        } else {
 
+        }
+        break
     }
 }
